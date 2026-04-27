c600b2718da9233f16813243182d0f275309bad9
Jonathan Leroy <jonathan@relkian.com>
2026-04-27 18:19:35 +0200

    Import skeleton

---
+++ tests/test_provider_octodns_opusdns.py
@@ -0,0 +1,14 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_opusdns import OpusDNSProvider
+
+
+class TestOpusDNSProvider(TestCase):
+    # TODO: test provider
+    def test_nothing(self):
+        self.assertTrue(True)
+        OpusDNSProvider
