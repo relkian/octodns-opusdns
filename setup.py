5c7d6ebecbc17d6a91c868d35043d6629df6cd35
Jonathan Leroy <jonathan@relkian.com>
2026-04-27 19:12:56 +0200

    Updates requirements

--- setup.py
+++ setup.py
@@ -20,12 +20,7 @@ def version():
 
 description, long_description = descriptions()
 
-tests_require = (
-    'pytest',
-    'pytest-cov',
-    'pytest-network',
-    # TODO: other test-time requirements
-)
+tests_require = ('pytest', 'pytest-cov', 'pytest-network')
 
 setup(
     author='Jonathan Leroy',
@@ -49,16 +44,13 @@ setup(
         ),
         'test': tests_require,
     },
-    install_requires=(
-        'octodns>=1.5.0',
-        # TODO: other requirements
-    ),
+    install_requires=('octodns>=1.16.0',),
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
     name='octodns-opusdns',
     packages=find_packages(),
-    python_requires='>=3.9',
+    python_requires='>=3.11',
     tests_require=tests_require,
     url='https://github.com/octodns/octodns-opusdns',
     version=version(),
