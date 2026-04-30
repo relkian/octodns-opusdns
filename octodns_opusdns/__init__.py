d623e82843beb061b927ab10c3773940afd87d30
Jonathan Leroy <jonathan@relkian.com>
2026-04-30 08:21:20 +0200

    Use octoDNS builtin Record.from_rrs() to handle raw records values instead of implementing one custom function per record type

--- octodns_opusdns/__init__.py
+++ octodns_opusdns/__init__.py
@@ -6,7 +6,7 @@ from requests import Session
 from octodns import __version__ as octodns_version
 from octodns.provider import ProviderException
 from octodns.provider.base import BaseProvider
-from octodns.record import Record
+from octodns.record import Record, Rr
 
 __version__ = '1.0.0'
 
@@ -156,14 +156,15 @@ class OpusDNSClient(object):
                     if rrset_name == zone_name:
                         rrset_name = ''
 
-                    record = {
-                        # "www.domain.tld." => "www".
-                        'name': rrset_name.removesuffix(f'.{zone_name}'),
-                        'type': rrset['type'],
-                        'ttl': rrset['ttl'],
-                        'values': [x['rdata'] for x in rrset['records']],
-                    }
-                    records.append(record)
+                    for v in rrset['records']:
+                        record = {
+                            # "www.domain.tld." => "www".
+                            'name': rrset_name.removesuffix(f'.{zone_name}'),
+                            'type': rrset['type'],
+                            'ttl': rrset['ttl'],
+                            'value': v['rdata'],
+                        }
+                        records.append(record)
 
                 self._zones[zone_name] = records
 
@@ -254,186 +255,6 @@ class OpusDNSProvider(BaseProvider):
         # Init OpusDNS client.
         self._client = OpusDNSClient(client_id, client_secret, sandbox)
 
-    def _data_for_single(self, _type, record):
-        # ALIAS: target.example.com.
-        # CNAME: target.example.com.
-        return {
-            'ttl': record['ttl'],
-            'type': _type,
-            'value': record['values'][0],
-        }
-
-    def _data_for_multiple(self, _type, record):
-        # A:    203.0.113.1
-        # AAAA: 2001:db8::1
-        # NS:   ns1.example.net.
-        # PTR:  rev-243.customers.example.com.
-        # TXT:  v=spf1 ip4:203.0.113.1/32 ip6:2001:db8::1/128 -all
-        return {'ttl': record['ttl'], 'type': _type, 'values': record['values']}
-
-    def _data_for_service_binding(self, _type, record):
-        values = []
-
-        for rvalue in record['values']:
-            # HTTPS: 0 target.example.com.
-            # HTTPS: 1 . alpn=h2,h3
-            # SVCB:  0 target.example.com.
-            # SVCB:  1 target.example.com. alpn="bar" port="8443".
-            svcpriority, targetname, *svcparams = rvalue.split(' ')
-            values.append(
-                {
-                    'svcpriority': svcpriority,
-                    'targetname': targetname,
-                    'svcparams': svcparams,
-                }
-            )
-
-        return {'ttl': record['ttl'], 'type': _type, 'values': values}
-
-    _data_for_A = _data_for_multiple
-    _data_for_AAAA = _data_for_multiple
-    _data_for_ALIAS = _data_for_single
-
-    def _data_for_CAA(self, _type, record):
-        values = []
-
-        for rvalue in record['values']:
-            # 0 issue "example.com; validationmethods=dns-01"
-            # 0 issuewild "example.com"
-            # 128 issue "example.org; accounturi=https://example.org/acct/1234"
-            flags, tag, value = rvalue.split(' ', 2)
-            values.append({'flags': flags, 'tag': tag, 'value': value})
-
-        return {'ttl': record['ttl'], 'type': _type, 'values': values}
-
-    _data_for_CNAME = _data_for_single
-
-    def _data_for_DS(self, _type, record):
-        values = []
-
-        for rvalue in record['values']:
-            # 8866 13 2 224da00fb6d0efd9799afee6[...]6b4503e3d3ce4bb4b76ff
-            key_tag, algorithm, digest_type, digest = rvalue.split(' ', 3)
-            values.append(
-                {
-                    'key_tag': key_tag,
-                    'algorithm': algorithm,
-                    'digest_type': digest_type,
-                    'digest': digest,
-                }
-            )
-
-        return {'ttl': record['ttl'], 'type': _type, 'values': values}
-
-    _data_for_HTTPS = _data_for_service_binding
-
-    def _data_for_MX(self, _type, record):
-        values = []
-
-        for rvalue in record['values']:
-            # 10 mail.example.com.
-            preference, exchange = rvalue.split(' ', 1)
-            values.append({'preference': preference, 'exchange': exchange})
-
-        return {'ttl': record['ttl'], 'type': _type, 'values': values}
-
-    def _data_for_NAPTR(self, _type, record):
-        values = []
-
-        for rvalue in record['values']:
-            # 100 10 "" "" "/urn:cid:.+@([^\.]+\.)(.*)$/\2/i" .
-            # 100 10 "U" "sip+E2U" "!^.*$!sip:bob@example.com!" .
-            # 100 10 "S" "SIP+D2U" "!^.*$!sip:bob@example.com!" _sip._udp.x.com.
-            order, preference, flags, service, regexp, replacement = (
-                rvalue.split(' ', 5)
-            )
-            values.append(
-                {
-                    'order': order,
-                    'preference': preference,
-                    'flags': flags,
-                    'service': service,
-                    'regexp': regexp,
-                    'replacement': replacement,
-                }
-            )
-
-        return {'ttl': record['ttl'], 'type': _type, 'values': values}
-
-    _data_for_NS = _data_for_multiple
-    _data_for_PTR = _data_for_multiple
-
-    def _data_for_SRV(self, _type, record):
-        values = []
-
-        for rvalue in record['values']:
-            # 0 1 993 imap.example.com.
-            priority, weight, port, target = rvalue.split(' ', 3)
-            values.append(
-                {
-                    'priority': priority,
-                    'weight': weight,
-                    'port': port,
-                    'target': target,
-                }
-            )
-
-        return {'ttl': record['ttl'], 'type': _type, 'values': values}
-
-    def _data_for_SSHFP(self, _type, record):
-        values = []
-
-        for rvalue in record['values']:
-            # 2 1 123456789abcdef67890123456789abcdef67890
-            algorithm, fingerprint, fingerprint_type = rvalue.split(' ', 2)
-            values.append(
-                {
-                    'algorithm': algorithm,
-                    'fingerprint': fingerprint,
-                    'fingerprint_type': fingerprint_type,
-                }
-            )
-
-        return {'ttl': record['ttl'], 'type': _type, 'values': values}
-
-    _data_for_SVCB = _data_for_service_binding
-
-    def _data_for_TLSA(self, _type, record):
-        values = []
-
-        for rvalue in record['values']:
-            # 1 1 2 92003ba34942dc74152[...]e51ffd48c43326cbc
-            (
-                certificate_usage,
-                selector,
-                matching_type,
-                certificate_association_data,
-            ) = rvalue.split(' ', 3)
-            values.append(
-                {
-                    'certificate_usage': certificate_usage,
-                    'selector': selector,
-                    'matching_type': matching_type,
-                    'certificate_association_data': certificate_association_data,
-                }
-            )
-
-        return {'ttl': record['ttl'], 'type': _type, 'values': values}
-
-    _data_for_TXT = _data_for_multiple
-
-    def _data_for_URI(self, _type, record):
-        values = []
-
-        for rvalue in record['values']:
-            # 10 1 "ftp://ftp1.example.com/public"
-            priority, weight, target = rvalue.split(' ', 2)
-            values.append(
-                {'priority': priority, 'weight': weight, 'target': target}
-            )
-
-        return {'ttl': record['ttl'], 'type': _type, 'values': values}
-
     def zone_records(self, zone):
         try:
             return self._client.zone(zone.name)
@@ -454,27 +275,32 @@ class OpusDNSProvider(BaseProvider):
         )
 
         before = len(zone.records)
-        zones_list = self.list_zones()
-        exists = zone.name in zones_list
+        exists = zone.name in self.list_zones()
 
+        # Create octoDNS Resource Record (Rr()) objects from zone data so we
+        # don't have to parse each RR type individually, as OpusDNS API returns
+        # raw RR values.
+        rrs = []
         for record in self.zone_records(zone):
-            _type = record['type']
+            record_type = record['type']
 
-            if _type not in self.SUPPORTS:
-                self.log.warning(
-                    'populate: skipping unsupported %s record', _type
-                )
+            if not record_type in self.SUPPORTS:
+                # Don't print this warning for SOA records.
+                if not record_type == 'SOA':
+                    self.log.warning(
+                        'populate: skipping unsupported %s record', record_type
+                    )
+
                 continue
 
-            data_for = getattr(self, f'_data_for_{_type}')
-            r = Record.new(
-                zone,
-                record['name'],
-                data_for(_type, record),
-                source=self,
-                lenient=lenient,
+            rrs.append(
+                Rr(record['name'], record_type, record['ttl'], record['value'])
             )
-            zone.add_record(r, lenient=lenient)
+
+        # Record.from_rrs() converts Rr() objects to octoDNS records
+        # (ARecord, AaaaRecord...), parsing RFC-formated records values.
+        for record in Record.from_rrs(zone, rrs, lenient=lenient):
+            zone.add_record(record, lenient=lenient)
 
         self.log.info(
             'populate:   found %s records, exists=%s',
